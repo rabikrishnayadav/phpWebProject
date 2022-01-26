c842a10e8c9aff4bb79a341fb1cc2b6e09536696
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-01-27 00:10:24 +0545

    content add

--- index.php
+++ index.php
@@ -10,5 +10,15 @@ include('links.php');
 <div id="image" class="container m-auto p-0">
 	<img src="assets/images/banner.jpg" alt="banner" class="img-fluid" style="height:300px; width:100%; margin:0 auto; ">
 </div>
-<div id="content" class="container"></div>
+<div id="content" class="container pt-3">
+	<h3 class="font-weight-bolder">what is HTML?</h3>
+	<ul type="circle">
+		<li>HTML stand for hyper text Markup Language.</li>
+		<li>HTML is a structure Layer.</li>
+		<li>HTML is a Client side Scripting Language.</li>
+		<li>Extension of HTML Web page is .html and .htm</li>
+		<li>HTML is used for create a structure of website.</li>
+		<li>HTML is used for print a text, images, videos and create apartition of web page.</li>
+	</ul>
+</div>
 <footer id="footer"></footer>
