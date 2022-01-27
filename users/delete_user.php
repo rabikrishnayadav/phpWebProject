d8edaf7dfe1d880307058c0754f04fc8475e816d
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-01-27 22:50:26 +0545

    multiple user delete with image from database using checkbox

--- users/delete_user.php
+++ users/delete_user.php
@@ -19,11 +19,24 @@ if (isset($_REQUEST['multi_delete'])) {
 
 	$array_to_string = implode(',',$delete_req); // these will change from array to string
 
+	// in whereClause 'where id in()' is used for search multiple ids
+	$sqlSearchQuery = "select * from users where id in($array_to_string)";
+
+	$sql_query = mysqli_query($con,$sqlSearchQuery);
+
+	$count_row = mysqli_num_rows($sql_query);
+
+	for($rows=1; $rows <= $count_row; $rows++){
+
+		$cols_data = mysqli_fetch_array($sql_query);
+		
+		unlink("../".$cols_data['photo']); // this will delete photo from directory
+
+	}
 	// in whereClause 'where id in()' is used for delete multiple ids
 	$sqlDeleteQuery = "delete from users where id in($array_to_string)";
 
 	$sql_query = mysqli_query($con,$sqlDeleteQuery);
 
 	header('location:user_list.php'); 
-
 }
