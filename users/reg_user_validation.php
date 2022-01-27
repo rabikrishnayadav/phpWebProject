553bc93a6dc5587772bb7cf7656f3669a084e199
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-01-27 16:15:59 +0545

    file upload on directory

--- users/reg_user_validation.php
+++ users/reg_user_validation.php
@@ -13,6 +13,22 @@ if (isset($_REQUEST['submit'])) {
 	// checkbox data will come in array for so convert it into string for storing in database
 	$con_arr_to_str  = implode(',', $education);
 
+	// file data will come in array form with carry many information so we can collect every array data on different variable for make some validation
+
+	$file = $_FILES['photo']; // here will come file every data in array form
+
+	$fileName = $_FILES['photo']['name'];			// here will come only file name
+	$fileType = $_FILES['photo']['type'];			// here will come only file type
+	$fileTmpName = $_FILES['photo']['tmp_name'];	// here will come only file temporary name
+	$fileError = $_FILES['photo']['error'];			// here will come only file Error
+	$fileSize = $_FILES['photo']['size'];			// here will come only file Size
+
+	// file upload directory name
+	$filePathName = '../assets/images/users/'.$fileName;
+
+	// sql query for upload file on dir.
+	$sqlFileUploadDir = move_uploaded_file($fileTmpName, $filePathName);
+
 	// data insert into databse
 	$sqlInsertQuery = "insert into users(username,password,address,gender,education,country) values('$username','$pwd','$address','$gender','$con_arr_to_str','$country')";
 
