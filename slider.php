d825090fd664b53c0c80345df875dccb03c1cc87
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-01-27 10:19:42 +0545

    slider page create

---
+++ slider.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title></title>
+		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap-grid.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap-grid.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap-reboot.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap-reboot.min.css">
+	</head>
+	<body>
+		<!-- <div class="container"> -->
+			<div class="carousel slide" id="myslider" data-ride="carousel">
+				<div class="carousel-inner" style="height:450px;">
+					<div class="carousel-item">
+						<img src="assets/images/banner.jpg" class="img-fluid" alt="images">
+					</div>
+					<div class="carousel-item active">
+						<img src="assets/images/banner1.jpg" class="img-fluid" alt="images">
+					</div>
+					<div class="carousel-item ">
+						<img src="assets/images/banner2.jpeg" class="img-fluid" alt="images">
+					</div>
+					<div class="carousel-item">
+						<img src="assets/images/banner3.jpg" class="img-fluid" alt="images">
+					</div>
+				</div>
+				<ul class="carousel-indicators">
+					<li data-target="#myslider" data-slide-to="0" class="active"></li>
+					<li data-target="#myslider" data-slide-to="1"></li>
+					<li data-target="#myslider" data-slide-to="2"></li>
+					<li data-target="#myslider" data-slide-to="3"></li>
+				</ul>
+				<a href="#myslider" data-slide="prev" class="carousel-control-prev">
+					<span class="carousel-control-prev-icon"></span>
+				</a>
+				<a href="#myslider" data-slide="next" class="carousel-control-next">
+					<span class="carousel-control-next-icon"></span>
+				</a>
+			</div>
+		</div>
+		<script type="text/javascript" src="assets/js/jquery-3.6.0.min.js"></script>
+		<script type="text/javascript" src="assets/js/bootstrap.js"></script>
+		<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="assets/js/bootstrap.bundle.js"></script>
+		<script type="text/javascript" src="assets/js/bootstrap.bundle.min.js"></script>
+		
+	</body>
+</html>
