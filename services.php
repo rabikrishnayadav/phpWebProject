047b5c05ef44d36f9317a1a79095eedd18f9f342
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-01-27 10:30:59 +0545

    some content add on service page

--- services.php
+++ services.php
@@ -11,11 +11,21 @@ include('links.php');
 	<img src="assets/images/banner.jpg" alt="banner" class="img-fluid" style="height:300px; width:100%; margin:0 auto; ">
 </div>
 <div id="content" class="container">
-	<div class="row" style="height: 200px">
-		<div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 bg-primary"></div>
+	<div class="row">
+		<div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 bg-primary">
+			<h3>what is PHP?</h3>
+			<ul>
+				<li>PHP Stands for Hypertext Preprocor.</li>
+				<li>It is a server side scripting language.</li>
+				<li>It is used for building dynamic web sites.</li>
+				<li>PHP programs run on web server.</li>
+				<li>PHP was created by Rasmus Lerdorf in 1994.</li>
+				<li>PHP is that you can embed php code within HTML Web pages.</li>
+				<li>PHP is Open Source.</li>
+			</ul>
+		</div>
 		<div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 bg-danger"></div>
 		<div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 bg-warning"></div>
 	</div>
-	
 </div>
 <footer id="footer"></footer>
