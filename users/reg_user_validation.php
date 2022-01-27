36cb6dc528991286646e396c85432238675a3b5a
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-01-27 15:30:26 +0545

    checkbox data save in database

--- users/reg_user_validation.php
+++ users/reg_user_validation.php
@@ -8,10 +8,13 @@ if (isset($_REQUEST['submit'])) {
 	$pwd = $_REQUEST['password'];
 	$address = $_REQUEST['address'];
 	$gender = $_REQUEST['gender'];
+	$education = $_REQUEST['education'];
+
+	// checkbox data will come in array for so convert it into string for storing in database
+	$con_arr_to_str  = implode(',', $education);
 
 	// data insert into databse
-
-	$sqlInsertQuery = "insert into users(username,password,address,gender) values('$username','$pwd','$address','$gender')";
+	$sqlInsertQuery = "insert into users(username,password,address,gender,education) values('$username','$pwd','$address','$gender','$con_arr_to_str')";
 
 	$sql_query = mysqli_query($con,$sqlInsertQuery);
 }
